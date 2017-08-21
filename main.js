258ab8600cde8bb62c626d75dc12400d20c8d0e4
Diana Ibarra <dibarra88@gmail.com>
2017-08-21 13:16:03 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+//###################################################################
+//        __
+//   ____/ /___  ____ ______
+//  / __  / __ \/ __ `/ ___/
+// / /_/ / /_/ / /_/ (__  )
+// \__,_/\____/\__, /____/
+//            /____/
+
+// Dog Constructor & Prototype
+function Dog (status, color, hungry, owner) {
+    this.status = status;
+    this.color = color;
+    this.hungry = hungry;
+    this.owner = owner;
+}
+
+// Instances of Dog
+// Needed: sadie, moonshine, atticus
+let sadie = new Dog('normal','black',false, 'mason');
+let moonshine = new Dog('','',true);
+let atticus = new Dog;
+//     __
+//    / /_  __  ______ ___  ____ _____  _____
+//   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
+//  / / / / /_/ / / / / / / /_/ / / / (__  )
+// /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
+
+// Human Constructor & Prototype
+function Human (cool) {
+    this.cool = cool;
+}
+Human.prototype.pet = function(dog){
+    dog.status = 'happy';
+}
+Human.prototype.feed = function(dog){
+    dog.hungry = false;
+}
+// Instances of Human
+// Needed: mason, julia
+let mason = new Human(false, sadie);
+let julia = new Human(true);
